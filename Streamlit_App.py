b45daf323621720915b7642819bb5be9ddfd0763
ChekriKhalife <145865707+ChekriKhalife@users.noreply.github.com>
2023-11-25 22:46:13 +0200

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -280,6 +280,7 @@ def main():
     st.title('NutriSwap Kitchen')
 
     st.sidebar.title("Features")
+    st.image("Nutriswap logo.jpg", width=300)
     option = st.sidebar.radio("Choose a feature", 
                               ["Home", "BMI Calculator", "Eat Healthy", "Educational Content", 
                                "Connect with a Nutritionist", "Healthy Food Provider", 
